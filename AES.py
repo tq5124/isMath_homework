6f2a8a1f376b4818f5e2aa150c6bbb87bfb4a392
傅雨东 <fengkuangxiaxia@sjtu.edu.cn>
2013-11-20 21:56:20 +0800

    第一次10万次乘法的测试，用时约5s，怀疑是封装成类导致的效率低下

--- AES.py
+++ AES.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
-import math
+import math,random
 
 class Pol_domain:
     #值
@@ -42,16 +42,29 @@ class Pol_domain:
     #标准化函数：使值为不超过不可约多项式的多项式
     def standardize(self):
         temp_val = self.val
-        val_b_len = int (math.ceil(math.log(temp_val,2)))
-        pol_b_len = int (math.ceil(math.log(self.pol,2)))
+        val_b_len = get_num_len(temp_val)
+        pol_b_len = get_num_len(self.pol)
         while(val_b_len >= pol_b_len):
             temp_val = temp_val ^ (self.pol << (val_b_len - pol_b_len))
-            val_b_len = int (math.ceil(math.log(temp_val,2)))
+            val_b_len = get_num_len(temp_val)
         self.val = temp_val
         return Pol_domain(temp_val,self.pol)
 
+def get_num_len(num):
+    num = int(num)
+    i = 0
+    if (num == 0):
+        i = 1
+    while (num > 0):
+        i = i + 1
+        num = num / 2
+    return i
 def main():
-    a = Pol_domain(282)
-    b = Pol_domain(3)
-    c = a * b
-    print c.val
+    file_object = open('test.txt', 'w+')
+    i = 0
+    while(i < 100000):
+        a = int (random.random() * 256)
+        b = int (random.random() * 256)
+        file_object.write("a = " + str(a) + "," + "b = " + str(b) + "," + "a * b = " + str((Pol_domain(a) * Pol_domain(b)).val) +"\n")
+        i = i + 1
+    file_object.close()
